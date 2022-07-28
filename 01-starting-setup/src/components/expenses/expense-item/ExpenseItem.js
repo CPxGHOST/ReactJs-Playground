40fd45c569c58ca6719ff14e2c800fb5c6f8d34c
HypertextAssassin <mridul2598@gmail.com>
2022-07-29 00:12:00 +0530

    Application Complete

--- 01-starting-setup/src/components/expenses/expense-item/ExpenseItem.js
+++ 01-starting-setup/src/components/expenses/expense-item/ExpenseItem.js
@@ -8,8 +8,9 @@ const ExpenseItem = (props) => {
     <li>
       <Card className="expense-item">
         <ExpenseDate date={props.expense.date} />
-        <div className="expense-item__description"></div>
-        <h2>{props.expense.title}</h2>
+        <div className="expense-item__description">
+          <h2>{props.expense.title}</h2>
+        </div>
         <div className="expense-item__price">${props.expense.amount}</div>
       </Card>
     </li>
