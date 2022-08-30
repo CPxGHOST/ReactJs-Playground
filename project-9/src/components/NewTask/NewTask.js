3adec4ff738d177a78b38e4fbd8fa03461ecb8b7
CPxGHOST <mridul2598@gmail.com>
2022-08-30 23:23:42 +0530

    Updated

--- project-9/src/components/NewTask/NewTask.js
+++ project-9/src/components/NewTask/NewTask.js
@@ -1,29 +1,22 @@
-import { useEffect, useState } from "react";
 import useApi from "../../hooks/UseApi";
-
 import Section from "../UI/Section";
 import TaskForm from "./TaskForm";
 
 const NewTask = (props) => {
-  const [taskText, setTaskText] = useState("");
-  const requestConfig = {
-    url: "https://custom-hook-http-f63df-default-rtdb.firebaseio.com/tasks.json",
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: { text: taskText },
-  };
-  const { isLoading, error, sendRequest } = useApi(requestConfig, UseData);
+  const { isLoading, error, sendRequest } = useApi(UseData);
 
-  useEffect(() => {
-    console.log("NewTask useEffect triggered!");
-    if (taskText.length > 0) sendRequest(requestConfig, UseData);
-  }, [taskText]);
   const enterTaskHandler = async (enteredTaskText) => {
-    setTaskText(enteredTaskText);
+    const requestConfig = {
+      url: "https://custom-hook-http-f63df-default-rtdb.firebaseio.com/tasks.json",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: { text: enteredTaskText },
+    };
+    sendRequest(requestConfig, UseData.bind(null, enteredTaskText));
   };
-  function UseData(data) {
+  function UseData(taskText, data) {
     const generatedId = data.name; // firebase-specific => "name" contains generated id
     const createdTask = { id: generatedId, text: taskText };
     props.onAddTask(createdTask);
